fc8794bb5be0db9084d23da2e00c7a22d31b6fa4
Sowon Park <thdnjs957@gmail.com>
2019-06-12 13:11:02 +0900

    Initial commit

---
+++ hello.py
@@ -0,0 +1,35 @@
+# hello.py
+
+
+def add(m, n):
+    s = m
+    s += n
+    return s
+
+
+def mymax(m, n):
+    if m > n:
+        return m
+    else:
+        return n
+
+
+a = 2
+b = 1
+
+if a > 1:
+    print("big")
+    print("yes")
+
+    for i in range(1,10):
+        print('-->',i)
+
+
+print('no')
+
+print('end')
+
+print('hello world!!')
+
+
+
